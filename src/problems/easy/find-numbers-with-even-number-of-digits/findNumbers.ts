0b85a3b403b55e063c4ac850faa5cc4620112db7
xiezipei <zipeixie@gmail.com>
2021-04-15 16:24:22 +0800

    update: findNumbers

--- src/problems/easy/find-numbers-with-even-number-of-digits/findNumbers.ts
+++ src/problems/easy/find-numbers-with-even-number-of-digits/findNumbers.ts
@@ -1,9 +1,18 @@
+/**
+ * 统计位数为偶数的数字
+ * 
+ * @problem
+ * https://leetcode-cn.com/problems/find-numbers-with-even-number-of-digits/
+ * 
+ * @param nums 数字数组
+ * @returns 数字
+ */
 export function findNumbers(nums: number[]): number {
-    let count = 0;
-    for (let i = 0; i < nums.length; i++) {
-        const str = nums[i].toString();
-        if (str.length % 2 === 0) {
-            count++;
+    let count = 0;  // 定义一个计数器变量
+    for (let i = 0; i < nums.length; i++) { // 遍历数组，每次得到元素i
+        const str = nums[i].toString(); // 把元素i转为字符串s
+        if (str.length % 2 === 0) { // 根据字符串s的长度判断是否为偶数位
+            count++;    // 元素为偶数位则计数器加一
         }
     }
     return count;
